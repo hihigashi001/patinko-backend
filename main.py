594dc4285a68171462b75a31c2ab4cc762678c38
higashionna <hihigashi001@gmial.com>
2022-02-06 16:05:28 +0900

    cross add

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 """This is a main program."""
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import controller
 
 app = FastAPI()
 
 app.include_router(controller.router)
 
+
+app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 if __name__ == "__main__":
-    uvicorn.run("main:app", port=8000, reload=True)
+    uvicorn.run("main:app", port=8000, reload=False)
